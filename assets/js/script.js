e0d3932c582790737c107ed3f3fb17375478c009
Bumbi <annika.borgesson@gmail.com>
2023-10-16 21:19:46 +0200

    Started targeting elements in js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let summaryEl = document.querySelector(".balance-box .total-value");
+
+let totalBudget = document.querySelector("budget-total");
+
+let totalActual = document.querySelector("actual-total");
+
+let budgetIncomeBtn = document.querySelector(".list1");
+let budgetCostsBtn = document.querySelector(".list2");
+
+let budgetCostsEl = document.querySelector("budget-costs");
+let budgetIncomeEl = document.querySelector("budget-incomes");
+
+let budgetCostList = document.querySelector("#budget-costs .budget-list");
+let budgetCostsList = document.querySelector("#budget-icomes .budget-list");
