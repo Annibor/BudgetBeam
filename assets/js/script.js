93045023d34c4377367ca1696bbb84cc0b68c691
Bumbi <annika.borgesson@gmail.com>
2023-10-17 09:35:17 +0200

    Add comments, selected actual cost & income list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,62 @@
+/*-------------------------*/
+/* --- SELECT ELEMENTS --- */
+/*-------------------------*/
+
+/* --- SUMMARY OF BUDGET AND ACTUAL COSTS --- */
 let summaryEl = document.querySelector(".balance-box .total-value");
-
 let totalBudget = document.querySelector("budget-total");
-
 let totalActual = document.querySelector("actual-total");
 
+
+/* --- BUDGET LIST --- */
 let budgetIncomeBtn = document.querySelector(".list1");
 let budgetCostsBtn = document.querySelector(".list2");
 
-let budgetCostsEl = document.querySelector("budget-costs");
-let budgetIncomeEl = document.querySelector("budget-incomes");
+/* --- COST LIST BUDGET COSTS --- */
+let budgetCostsEl = document.getElementById("#budget-costs");
 
+/* --- COST LIST BUDGET INCOME --- */
+let budgetIncomeEl = document.getElementById("#budget-incomes");
+
+/* --- BUDGET INCOME AND COST LIST --- */
 let budgetCostList = document.querySelector("#budget-costs .budget-list");
-let budgetCostsList = document.querySelector("#budget-icomes .budget-list");
+let budgetCostsList = document.querySelector("#budget-incomes .budget-list");
 
-/*-------------------------*/
 /* --- BUDGET COST ADD --- */
-/*-------------------------*/
 let budgetCostInput = document.getElementById("#budget-cost-add");
 let budgetCostAmount = document.getElementById("#budget-cost-amount");
 let addBudgetCostBtn = document.querySelector(".add-cost-btn");
 
-/*---------------------------*/
 /* --- BUDGET INCOME ADD --- */
-/*---------------------------*/
 let budgetIncomeInput = document.getElementById("#budget-income-add");
 let budgetIncomeAmount = document.getElementById("#budget-income-amount");
 let addBudgetIncomeBtn = document.querySelector(".add-income-btn");
 
-/*-------------------------*/
+/* --- ACTUAL LIST --- */
+let actualIncomeBtn = document.getElementById("#actual-list1");
+let actualCostsBtn = document.getElementById("#actual-list2");
+
+/* --- COST LIST ACTUAL COSTS --- */
+let actualCostsEl = document.getElementById("#actual-costs");
+
+/* --- COST LIST ACTUAL INCOME --- */
+let actualIncomeEl = document.getElementById("#actual-incomes");
+
+/* --- ACTUAL INCOME AND COST LIST --- */
+let actualCostList = document.querySelector("#actual-costs .actual-list");
+let actualCostsList = document.querySelector("#actual-incomes .actual-list");
+
 /* --- ACTUAL COST ADD --- */
-/*-------------------------*/
 let actualCostInput = document.getElementById("#actual-cost-add");
 let actualCostAmount = document.getElementById("#actual-cost-amount");
 let addActualCostBtn = document.querySelector(".add-actual-cost-btn");
 
-/*---------------------------*/
 /* --- ACTUAL INCOME ADD --- */
-/*---------------------------*/
-let actualIncomeInput = document.getElementById("#budget-income-add");
-let actualIncomeAmount = document.getElementById("#budget-income-amount");
+let actualIncomeInput = document.getElementById("#actual-income-add");
+let actualIncomeAmount = document.getElementById("#actual-income-amount");
 let addActualIncomeBtn = document.querySelector(".add-actual-income-btn");
+
+/*---------------------------*/
+/* ---  --- */
+/*---------------------------*/
+
